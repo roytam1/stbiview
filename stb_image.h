2d08baf41945fbe0b2722eb81d1ca7fac5658abc
roytam1 <roytam@gmail.com>
2026-07-27 21:10:34 +0800

    stb_image: add -DSTBI_DONT_USE_SECURE_CRT_FUNCTIONS for NOT using secure crt functions

--- stb_image.h
+++ stb_image.h
@@ -1350,14 +1350,14 @@ static FILE *stbi__fopen(char const *filename, char const *mode)
 	if (0 == MultiByteToWideChar(65001 /* UTF8 */, 0, mode, -1, wMode, sizeof(wMode)/sizeof(*wMode)))
       return 0;
 
-#if defined(_MSC_VER) && _MSC_VER >= 1400
+#if defined(_MSC_VER) && _MSC_VER >= 1400 && !defined(STBI_DONT_USE_SECURE_CRT_FUNCTIONS)
 	if (0 != _wfopen_s(&f, wFilename, wMode))
 		f = 0;
 #else
    f = _wfopen(wFilename, wMode);
 #endif
 
-#elif defined(_MSC_VER) && _MSC_VER >= 1400
+#elif defined(_MSC_VER) && _MSC_VER >= 1400 && !defined(STBI_DONT_USE_SECURE_CRT_FUNCTIONS)
    if (0 != fopen_s(&f, filename, mode))
       f=0;
 #else

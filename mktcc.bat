1572bec424d651ae6dc1f906f2967462b3e3062c
roytam1 <roytam@gmail.com>
2026-07-27 21:09:41 +0800

    mktcc: set WINVER and _WIN32_WINNT to 0x400 for NT 4 compatibility

--- mktcc.bat
+++ mktcc.bat
@@ -1,6 +1,6 @@
 set TCCPATH=F:\tinycc-win32
 set TCCLPATH=%TCCPATH%\lib
-%TCCPATH%\tcc -O2 picview.c %TCCLPATH%\wincrt1-crtdll.c %TCCLPATH%\crtdll-chkstk.S %TCCLPATH%\udivdi3.S %TCCLPATH%\umoddi3.S %TCCLPATH%\libm.c -nostdlib -lkernel32 -lcrtdll -lcomdlg32 -lshell32 -lgdi32 -luser32
-%TCCPATH%\LinkRes2Exe picview.res picview.exe
+%TCCPATH%\tcc -O2 -DWINVER=0x0400 -D_WIN32_WINNT=0x400 picview.c %TCCLPATH%\wincrt1-crtdll.c %TCCLPATH%\crtdll-chkstk.S %TCCLPATH%\udivdi3.S %TCCLPATH%\umoddi3.S %TCCLPATH%\libm.c -nostdlib -lkernel32 -lcrtdll -lcomdlg32 -lshell32 -lgdi32 -luser32 -o picview-tcc.exe
+%TCCPATH%\LinkRes2Exe picview.res picview-tcc.exe
 set TCCPATH=
 set TCCLPATH=
